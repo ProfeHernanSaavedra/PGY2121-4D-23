dcca812ddb3e70fb660de814cf8ad348c6251dc3
USRVI-LC3 <USRVI-LC3@LC060300.duoc.acad>
2023-08-30 15:11:25 -0400

    revision clase DATE

---
+++ src/ejericcio132/Fecha.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejericcio132;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author USRVI-LC3
+ */
+public class Fecha {
+    
+    public static void main(String[] args) {
+        
+        Date fecha = new Date();
+        System.out.println(fecha);
+        SimpleDateFormat formato = new SimpleDateFormat("dd-MM-yyyy");
+        System.out.println(formato.format(fecha));
+        
+    }
+    
+}
