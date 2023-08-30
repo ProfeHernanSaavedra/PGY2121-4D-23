f0f06bb05418efe694eadbd1d0b1196564fa1cb2
USRVI-LC3 <USRVI-LC3@LC060300.duoc.acad>
2023-08-30 15:02:10 -0400

    ejercicio 132 parte 2 y final

--- src/ejericcio132/Principal.java
+++ src/ejericcio132/Principal.java
@@ -1,5 +1,8 @@
 package ejericcio132;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 /**
@@ -89,8 +92,113 @@ public class Principal {
                     System.out.println(cliente);
                     System.out.println("------------------------");
                     break;
+                case 2:
+                    //solicitud
+                    numero = sol.obtenerNumeroSolicitud();
+                    sol.setNumero(numero);
+                    sol.setCliente(cliente);
+                    Date fechaDate = null;
+                    do {
+                        System.out.println("Ingrese Fecha (dd-mm-yyyy)");
+                        fecha = leer.next();
+                        SimpleDateFormat formato = new SimpleDateFormat("dd-MM-yyyy");
+                        
+                        //trycatch + tab
+                        try {
+                            fechaDate = formato.parse(fecha);
+                            flag = valida.validarFecha(fechaDate);
+                        } catch (ParseException e) {
+                            flag = false;
+                            System.out.println("Fecha no valida (dd-mm-yyyy)");
+                        }
+                        
+                    } while (flag == false);
+                    sol.setFecha(fechaDate);
                     
+                    //productos
+                    System.out.println("¿Cuantos productos ingresara?");
+                    totalProductos = leer.nextInt();
+                    for (int i = 1; i <= totalProductos; i++) {
+                        
+                        Producto prod = new Producto();
+                        codigo = 0;
+                        precio = 0;
+                        cantidad = 0;
+                        subcategoria = "";
+                        descripcion = "";
+                        medida  = ' ';
+                        
+                        do {
+                            System.out.println("Ingrese codigo producto");
+                            codigo = leer.nextInt();
+                            flag = valida.validarLargo4(codigo);
+                            if (flag == false) {
+                                System.out.println("codigo debe ser largo 4");
+                            }
+                        } while (flag == false);
+                        
+                        do {
+                            System.out.println("Ingrese Precio:");
+                            precio = leer.nextInt();
+                            flag = valida.validarNumeroPositivo(precio);
+                            if (flag == false) {
+                                System.out.println("El precio deb ser positivo");
+                            }
+                        } while (flag == false);
+                        
+                        do {
+                            System.out.println("Ingrese subcategoria (Fruta o Verdura)");
+                            subcategoria = leer.next();
+                            flag = valida.validarSubcategoria(subcategoria);
+                            if (flag==false) {
+                                System.out.println("subcategoria invalida");
+                            }
+                        } while (flag == false);
+                        
+                        System.out.println("Ingrese descripcion:");
+                        descripcion = leer.next();
+                        
+                        System.out.println("Ingrese unidad de medida (U:unidad, G:gramos)");
+                        medida = leer.next().toUpperCase().charAt(0);
+                        
+                        if (medida == 'U') {
+                            System.out.println("Ingrese unidades");
+                        } else {
+                            System.out.println("Ingrese gramos");
+                        }
+                        
+                        cantidad = leer.nextInt();
+                        
+                        prod.setCodigo(codigo);
+                        prod.setPrecio(precio);
+                        prod.setSubcategoria(subcategoria);
+                        prod.setDescripcion(descripcion);
+                        prod.setMedida(medida);
+                        prod.setCantidad(cantidad);
+                        
+                        sol.agregarProductos(prod);
+                        System.out.println("***********************");
+                        System.out.println(sol);
+                    }
+                    
+                    break;
+                
+                case 3:
+                    System.out.println("Ingrese procentaje de descuento 1-100%");
+                    porcentaje = leer.nextInt();
+                    descuento = sol.descuento(porcentaje);
+                    System.out.println("Total $" + sol.getTotal());
+                    System.out.println("Descuento $" + descuento);
+                    System.out.println("---------------------");
+                    System.out.println("a pagar $ "+ (sol.getTotal()-descuento));
+                    break;
+                case 4:
+                    System.out.println("");
+                    System.out.println("Solcitud terminada");
+                    sol.verSolicitud();
+                    break;
                 default:
+                    System.out.println("Opcion no valida");
                     
             }
             
